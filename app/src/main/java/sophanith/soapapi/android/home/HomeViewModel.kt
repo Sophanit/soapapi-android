63b88a63848fc4cd0f9276d8b2d1298e9ae5ca27
Sophanit <sophanit@Apples-MacBook.local>
2018-03-09 11:21:40 +0700

    Soap api

---
+++ app/src/main/java/sophanith/soapapi/android/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package sophanith.soapapi.android.home
+
+import android.content.Context
+import sophanith.soapapi.android.BaseViewModel
+
+/****
+ *
+ * Created by TUON BONDOL on 12/5/17.
+ *
+ */
+
+class HomeViewModel(context: Context) : BaseViewModel(context) {
+}
